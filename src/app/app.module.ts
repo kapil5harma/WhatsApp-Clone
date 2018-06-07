e834b1ec9c662d0a268bcc802d4c0bc34c7c148d
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-07 22:11:33 +0530

    MomentModule import from angular2-moment and used amCalendar pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { MomentModule } from 'angular2-moment';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [MyApp, ChatsPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), MomentModule],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, ChatsPage],
   providers: [
