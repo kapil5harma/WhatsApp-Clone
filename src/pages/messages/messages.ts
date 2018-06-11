9e87e0d40ff2dc60e763daf7a784d252db900628
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-11 23:11:15 +0530

    AutoScroll when new message comes

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Chat, Message, MessageType } from 'api/models';
 import { Observable } from 'rxjs';
@@ -10,14 +10,20 @@ import { MeteorObservable } from 'meteor-rxjs';
   selector: 'page-messages',
   templateUrl: 'messages.html'
 })
-export class MessagesPage implements OnInit {
+export class MessagesPage implements OnInit, OnDestroy {
   selectedChat: Chat;
   title: string;
   picture: string;
   messages: Observable<Message[]>;
   message: string = '';
+  autoScroller: MutationObserver;
+  scrollOffset = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private ele: ElementRef
+  ) {
     this.selectedChat = <Chat>navParams.get('chat');
     this.title = this.selectedChat.title;
     this.picture = this.selectedChat.picture;
@@ -25,7 +31,21 @@ export class MessagesPage implements OnInit {
     console.log('Selected chat is: ', this.selectedChat);
   }
 
+  private get messagesPageContent(): Element {
+    return this.ele.nativeElement.querySelector('.messages-page-content');
+  }
+
+  private get messagesList(): Element {
+    return this.messagesPageContent.querySelector('.messages');
+  }
+
+  private get scroller(): Element {
+    return this.messagesList.querySelector('.scroll-content');
+  }
+
   ngOnInit() {
+    this.autoScroller = this.autoScroll();
+
     let isEven = false;
 
     this.messages = Messages.find(
@@ -39,6 +59,28 @@ export class MessagesPage implements OnInit {
 
       return messages;
     });
+  }
+
+  ngOnDestroy() {
+    this.autoScroller.disconnect();
+  }
+
+  autoScroll(): MutationObserver {
+    const autoScroller = new MutationObserver(this.scrollDown.bind(this));
+
+    autoScroller.observe(this.messagesList, {
+      childList: true,
+      subtree: true
+    });
+
+    return autoScroller;
+  }
+
+  scrollDown(): void {
+    // Scroll down and apply specified offset
+    this.scroller.scrollTop = this.scroller.scrollHeight - this.scrollOffset;
+    // Zero offset for next invocation
+    this.scrollOffset = 0;
   }
 
   onInputKeypress({ keyCode }: KeyboardEvent): void {
