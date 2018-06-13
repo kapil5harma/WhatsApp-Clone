efb7ea268778e904e1492b46788dbe38341133bb
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-13 23:11:29 +0530

    Added verification component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
-import { MessagesPage } from './../pages/messages/messages';
-import { ChatsPage } from './../pages/chats/chats';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { MyApp } from './app.component';
+
 import { MomentModule } from 'angular2-moment';
 
-import { MyApp } from './app.component';
+import { ChatsPage } from './../pages/chats/chats';
+import { MessagesPage } from './../pages/messages/messages';
 import { PhoneService } from '../services/phone';
 import { LoginPage } from '../pages/login/login';
+import { VerificationPage } from './../pages/verification/verification';
 
 @NgModule({
-  declarations: [MyApp, ChatsPage, MessagesPage, LoginPage],
+  declarations: [MyApp, ChatsPage, MessagesPage, LoginPage, VerificationPage],
   imports: [BrowserModule, IonicModule.forRoot(MyApp), MomentModule],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, ChatsPage, MessagesPage, LoginPage],
+  entryComponents: [
+    MyApp,
+    ChatsPage,
+    MessagesPage,
+    LoginPage,
+    VerificationPage
+  ],
   providers: [
     StatusBar,
     SplashScreen,
