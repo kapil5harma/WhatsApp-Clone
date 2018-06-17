cc767956c89f3484eed8bb113dac761092933f6a
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-17 20:48:57 +0530

    Implement chats with with real data

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -8,9 +8,10 @@ import {
   PopoverController,
   ModalController
 } from 'ionic-angular';
-import { Chats, Messages } from './../../../api/server/collections';
-import { Chat } from 'api/models';
-import { Observable } from 'rxjs';
+import { Chats, Messages, Users } from './../../../api/server/collections';
+import { Chat, Message } from 'api/models';
+import { MeteorObservable } from 'meteor-rxjs';
+import { Observable, Subscriber } from 'rxjs';
 import { ChatOptionsComponent } from '../../components/chat-options/chat-options';
 
 @IonicPage()
@@ -20,13 +21,16 @@ import { ChatOptionsComponent } from '../../components/chat-options/chat-options
 })
 export class ChatsPage implements OnInit {
   chats;
+  senderId: string;
 
   constructor(
     private navCtrl: NavController,
     public navParams: NavParams,
     private popoverCtrl: PopoverController,
     private modalCtrl: ModalController
-  ) {}
+  ) {
+    this.senderId = Meteor.userId();
+  }
 
   addChat(): void {
     const modal = this.modalCtrl.create(NewChatComponent);
@@ -34,19 +38,68 @@ export class ChatsPage implements OnInit {
   }
 
   ngOnInit() {
-    this.chats = Chats.find({}).mergeMap((chats: Chat[]) =>
-      Observable.combineLatest(
-        ...chats.map((chat: Chat) =>
-          Messages.find({ chatId: chat._id })
-            .startWith(null)
-            .map(messages => {
-              if (messages) chat.lastMessage = messages[0];
-              return chat;
-            })
-        )
-      )
-    );
-    // .zone(); // Commented this line as it throws error.
+    this.chats = this.findChats();
+  }
+
+  findChats(): Observable<Chat[]> {
+    // Find chats and transform them
+    return Chats.find().map(chats => {
+      chats.forEach(chat => {
+        chat.title = '';
+        chat.picture = '';
+
+        const receiverId = chat.memberIds.find(
+          memberId => memberId !== this.senderId
+        );
+        const receiver = Users.findOne(receiverId);
+
+        if (receiver) {
+          chat.title = receiver.profile.name;
+          chat.picture = receiver.profile.picture;
+        }
+
+        // This will make the last message reactive
+        this.findLastChatMessage(chat._id).subscribe(message => {
+          chat.lastMessage = message;
+        });
+      });
+
+      return chats;
+    });
+  }
+
+  findLastChatMessage(chatId: string): Observable<Message> {
+    return Observable.create((observer: Subscriber<Message>) => {
+      const chatExists = () => !!Chats.findOne(chatId);
+
+      // Re-compute until chat is removed
+      MeteorObservable.autorun()
+        .takeWhile(chatExists)
+        .subscribe(() => {
+          Messages.find(
+            { chatId },
+            {
+              sort: { createdAt: -1 }
+            }
+          ).subscribe({
+            next: messages => {
+              // Invoke subscription with the last message found
+              if (!messages.length) {
+                return;
+              }
+
+              const lastMessage = messages[0];
+              observer.next(lastMessage);
+            },
+            error: e => {
+              observer.error(e);
+            },
+            complete: () => {
+              observer.complete();
+            }
+          });
+        });
+    });
   }
 
   showOptions(): void {
