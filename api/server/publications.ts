d2dba3b1bc50e56a25bea8b5ca29da0d5ee112c5
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-17 21:15:34 +0530

    1. Add users publication 2. Publish messages

---
+++ api/server/publications.ts
@@ -0,0 +1,33 @@
+import { User, Message } from './models';
+import { Users } from './collections/users';
+import { Messages } from './collections/messages';
+
+Meteor.publish('users', function(): Mongo.Cursor<User> {
+  if (!this.userId) {
+    return;
+  }
+
+  return Users.collection.find(
+    {},
+    {
+      fields: {
+        profile: 1
+      }
+    }
+  );
+});
+
+Meteor.publish('messages', function(chatId: string): Mongo.Cursor<Message> {
+  if (!this.userId || !chatId) {
+    return;
+  }
+
+  return Messages.collection.find(
+    {
+      chatId
+    },
+    {
+      sort: { createdAt: -1 }
+    }
+  );
+});
